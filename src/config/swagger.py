9194498f727ac05972f6a0f0d6066ec46e4539a0
Pavel Khramov <khramov_pd@devtrix.ru>
2024-01-20 15:51:41 +0500

    Add pre-commit

--- src/config/swagger.py
+++ src/config/swagger.py
@@ -1,5 +1,6 @@
 from fastapi.responses import ORJSONResponse
 
+
 app_config = dict(
     title="Language Flash Cards",
     docs_url="/docs",
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+
 from src.config import app_config
+
 
 app = FastAPI(**app_config)
