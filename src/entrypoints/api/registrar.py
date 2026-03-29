2839d4b13f6426a5772d379550e4e76378d8da18
Pavel Khramov <paul.khramov@gmail.com>
2026-03-29 10:12:12 +0500

    feature: dishka initial commit

--- src/entrypoints/api/registrar.py
+++ src/entrypoints/api/registrar.py
@@ -1,12 +1,16 @@
+from dishka import make_async_container
+from dishka.integrations.fastapi import setup_dishka
 from fastapi import FastAPI
 
 from src.core.config import Settings
+from src.infra.di.postgres import PostgresProvider
+from src.infra.di.settings import SettingsProvider
 
 
 def create_app(settings: Settings) -> FastAPI:
     """FastAPI app registration."""
 
-    return FastAPI(
+    app = FastAPI(
         title=settings.FASTAPI_TITLE,
         version=settings.FASTAPI_VERSION,
         description=settings.FASTAPI_DESCRIPTION,
@@ -14,3 +18,12 @@ def create_app(settings: Settings) -> FastAPI:
         redoc_url=settings.FASTAPI_REDOC_URL,
         openapi_url=settings.FASTAPI_OPENAPI_URL,
     )
+
+    # DI inject
+    container = make_async_container(
+        SettingsProvider(settings=settings),
+        PostgresProvider(),
+    )
+    setup_dishka(container, app)
+
+    return app
